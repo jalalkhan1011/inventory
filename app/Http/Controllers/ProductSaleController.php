c3f0eaca29372cc8d6ba65bfd32ec0742eba757c
“jalalkhan1011” <jalal.khan981@gmail.com>
2021-11-08 21:46:50 -0800

    product sale edit calculation done

--- app/Http/Controllers/ProductSaleController.php
+++ app/Http/Controllers/ProductSaleController.php
@@ -160,7 +160,7 @@ class ProductSaleController extends Controller
         $data = $request->all();
         $data['user_id'] = auth()->user()->id;
 
-//        $productSaleId->updatec($data);
+        $productSaleId->update($data);
 
         $saleItemId = ProductSaleItem::where('product_sale_id',$productSaleId->id)->select('product_id')->get()->toArray();//data get form table array format that why use toArray and update multiple data on by one
 
@@ -176,7 +176,7 @@ class ProductSaleController extends Controller
                     'qty' => $productFind['qty'] + $request->sale_qty[$i]
                 ];
 
-//                $productFind->update($data);
+                $productFind->update($data);
             }
         }
 
@@ -195,7 +195,7 @@ class ProductSaleController extends Controller
                     'sale_qty' => $saleItemFind['sale_qty'] - $request->sale_qty[$j]
                 ];
 
-//                $saleItemFiend->update($data);
+                $saleItemFind->update($data);
             }
 
         }
@@ -220,12 +220,65 @@ class ProductSaleController extends Controller
             }
         }
 
+        $saleItemIdDelete = DB::table('product_sale_items')->where('product_sale_id',$productSaleId->id)->delete();
+        $productId = count($_POST['product_id']);
+        for($i=0; $i<$productId; $i++){
+            ProductSaleItem::create([
+                'product_id' => $request->product_id[$i],
+                'customer_id' => $request->customer_id,
+                'category_id' => $request->category_id[$i],
+                'brand_id' => $request->brand_id[$i],
+                'stock_qty' => $request->stock_qty[$i],
+                'sale_qty' => $request->sale_qty[$i],
+                'sale_price' => $request->sale_price[$i],
+                'unit_id' => $request->unit_id[$i],
+                'total_item_price' => $request->total_item_price[$i],
+                'product_sale_id' => $productSaleId->id,
+                'user_id' => auth()->user()->id,
+                'created_at' => now()
+            ]);
+        }
+
+        $saleTransactionDelete = DB::table('product_transactions')->where('product_sale_id',$productSaleId->id)->delete();
+//        dd($saleTransactionDelete);
+        $saleProductId = ProductSaleItem::where('product_sale_id',$productSaleId->id)->select('product_id')->get()->toArray();//get array data form product item table
+
+        $productSaleQty = [];
+        foreach ($saleProductId as $row){
+            $productSaleQty[] = $row['product_id'];
+        }
+        foreach ( $productSaleQty as $k=>$saleQty ){
+            $saleQtyFind = Product::findOrFail($saleQty);
+
+            if($saleQtyFind['id']){
+                $data = [
+                    'qty' => $saleQtyFind['qty'] - $request->sale_qty[$k]
+                ];
+
+                $saleQtyFind->update($data );
+            }
+        }
+
+        for($j =0; $j<$productId; $j++){
+            ProductTransaction::create([
+                'product_id' => $request->product_id[$j],
+                'customer_id' => $request->customer_id,
+                'category_id' => $request->category_id[$j],
+                'brand_id' =>  $request->brand_id[$j],
+                's_qty' => $request->sale_qty[$j],
+                'stock_qty' => $request->stock_qty[$j] -  $request->sale_qty[$j],
+                's_unit_amount' => $request->sale_price[$j],
+                's_total_amount' => $request->total_item_price[$j],
+                'status' => 'S',
+                'product_sale_id' => $productSaleId->id,
+                'employee_id' => $employeeId->id,
+                'user_id' => auth()->user()->id,
+                'created_at' => now()
+            ]);
+        }
 
 
-
-
-
-        return  redirect()->back();
+       return  redirect()->back();
     }
 
     /**
