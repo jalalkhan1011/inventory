42dc6e910972461d494844dc1844102e9a94263d
“jalalkhan1011” <jalal.khan981@gmail.com>
2021-11-26 19:55:51 -0800

    side bar update

--- resources/views/admin/includes/sideBar.blade.php
+++ resources/views/admin/includes/sideBar.blade.php
@@ -11,11 +11,11 @@
 
 <ul class="sidenav-inner py-1">
     <li class="sidenav-item active">
-        <a href="index-2.html" class="sidenav-link">
+        <a href="{{route('home')}}" class="sidenav-link">
             <i class="sidenav-icon feather icon-home"></i>
             <div>Dashboards</div>
             <div class="pl-1 ml-auto">
-                <div class="badge badge-danger">Hot</div>
+                <div class="badge badge-danger"></div>
             </div>
         </a>
     </li>
@@ -122,53 +122,5 @@
                 </a>
             </li>
         </ul>
-    </li>
-    <li class="sidenav-item">
-        <a href="tables_bootstrap.html" class="sidenav-link">
-            <i class="sidenav-icon feather icon-grid"></i>
-            <div>Tables</div>
-        </a>
-    </li>
-
-    <li class="sidenav-divider mb-1"></li>
-    <li class="sidenav-header small font-weight-semibold">Icons</li>
-    <li class="sidenav-item">
-        <a href="javascript:" class="sidenav-link sidenav-toggle">
-            <i class="sidenav-icon feather icon-feather"></i>
-            <div>Icons</div>
-        </a>
-        <ul class="sidenav-menu">
-            <li class="sidenav-item">
-                <a href="icons_feather.html" class="sidenav-link">
-                    <div>Feather</div>
-                </a>
-            </li>
-            <li class="sidenav-item">
-                <a href="icons_linearicons.html" class="sidenav-link">
-                    <div>Linearicons</div>
-                </a>
-            </li>
-        </ul>
-    </li>
-
-    <li class="sidenav-divider mb-1"></li>
-    <li class="sidenav-header small font-weight-semibold">Pages</li>
-    <li class="sidenav-item">
-        <a href="pages_authentication_login-v1.html" class="sidenav-link">
-            <i class="sidenav-icon feather icon-lock"></i>
-            <div>Login</div>
-        </a>
-    </li>
-    <li class="sidenav-item">
-        <a href="pages_authentication_register-v1.html" class="sidenav-link">
-            <i class="sidenav-icon feather icon-user"></i>
-            <div>Signup</div>
-        </a>
-    </li>
-    <li class="sidenav-item">
-        <a href="pages_faq.html" class="sidenav-link">
-            <i class="sidenav-icon feather icon-anchor"></i>
-            <div>FAQ</div>
-        </a>
     </li>
 </ul>
